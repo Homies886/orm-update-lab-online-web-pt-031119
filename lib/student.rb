e17dcb08ba39617aaae007686c971af1d0e42188
Nicholas Rodriguez <niko876@hotmail.com>
2019-05-23 20:44:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,8 @@ class Student
     @grade = grade
   end
 
-  def self.create
-    student = Student.new
+  def self.create(name, grade)
+    student = Student.new(name, grade)
     student.save
     student
   end
